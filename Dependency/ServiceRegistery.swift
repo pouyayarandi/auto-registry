ff8f8e1f19304046ba9d95a72a35734f20ddb489
Pouya Yarandi <pouya.yarandi@divar.ir>
2024-03-12 15:11:06 +0330

    Add comment in auto-generated file

--- Dependency/ServiceRegistery.swift
+++ Dependency/ServiceRegistery.swift
@@ -1,7 +1,7 @@
+// AUTO-GENERATED, Please don't change it manually
 import Factory
 import MyLib_Wiring
 import CoreLibrary_Wiring
-
 extension Container {
     func registerDependencies() {
         MyLib.register(factory: MyLib_Wiring.build)
